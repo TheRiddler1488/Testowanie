8c84e5f71b37d586b21f35d4824b1602270413a7
studentwsb <eazyshotlocket@gmail.com>
2024-03-13 16:23:30 +0100

    onime-debug

--- testproject/urls.py
+++ testproject/urls.py
@@ -20,7 +20,11 @@ from django.urls import path
 def trigger_error(request):
     division_by_zero = 1 / 0
 
+def onime_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('sentry-debug/', trigger_error),
+    path('onime-debug/', onime_error),
 ]
